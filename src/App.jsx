e0115c9dbbf5abda4b2aeef7a0a997630c3c3ae7
dylanfox44 <131827295+dylanfox44@users.noreply.github.com>
2025-03-27 23:25:27 +0000

    new image

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,30 @@ const movie1 = {
   title: 'The Shawshank Redemption',
   director: 'Frank Darabont',
   release: 'October 14, 1994',
-  image: 'https://en.wikipedia.org/wiki/File:ShawshankRedemptionMoviePoster.jpg',
+  image: '/images/movie1.jpg',
   height: '378',
   width: '264'
 };
 
-function MovieList() {
+const movie2 = {
+  title: 'Inception',
+  director: 'Christopher Nolan',
+  release: 'July 13, 2010',
+  image: '/images/movie2.jpg',
+  height: '378',
+  width: '264'
+};
+
+const movie3 = {
+  title: 'The Shawshank Redemption',
+  director: 'Frank Darabont',
+  release: 'October 14, 1994',
+  image: '/images/movie1.jpg',
+  height: '378',
+  width: '264'
+};
+
+function Movie() {
   return (
     <div>
       <h2>{movie1.title} ({movie1.release})</h2>
@@ -29,12 +47,47 @@ function MovieList() {
   );
 }
 
+function Movie2() {
+  return (
+    <div>
+      <h2>{movie1.title} ({movie1.release})</h2>
+      <p>{movie1.director}</p>
+      <img className="moviePoster"
+      src={movie1.image}
+      alt={movie1.title + ' poster'}
+      style={{
+        width: movie1.width,
+        height: movie1.height
+      }}
+      />
+    </div>
+  );
+}
+
+function Movie3() {
+  return (
+    <div>
+      <h2>{movie1.title} ({movie1.release})</h2>
+      <p>{movie1.director}</p>
+      <img className="moviePoster"
+      src={movie1.image}
+      alt={movie1.title + ' poster'}
+      style={{
+        width: movie1.width,
+        height: movie1.height
+      }}
+      />
+    </div>
+  );
+}
 
 function App() {
   
   return (
     <>
-    <MovieList />
+    <Movie />
+    <Movie2 />
+    <Movie3 />
     </>
   )
 }
